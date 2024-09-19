3d9a18c8807868dc4be597d85550998ad60bdf71
Vipaloo <154497596+Vipaloo@users.noreply.github.com>
2024-09-19 10:31:01 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFolder1 = new System.Windows.Forms.Button();
+            this.labelPath1 = new System.Windows.Forms.Label();
+            this.labelPath2 = new System.Windows.Forms.Label();
+            this.btnFolder2 = new System.Windows.Forms.Button();
+            this.SelectFolder1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnFolder1
+            // 
+            this.btnFolder1.Location = new System.Drawing.Point(43, 29);
+            this.btnFolder1.Name = "btnFolder1";
+            this.btnFolder1.Size = new System.Drawing.Size(116, 45);
+            this.btnFolder1.TabIndex = 0;
+            this.btnFolder1.Text = "select folder 1";
+            this.btnFolder1.UseVisualStyleBackColor = true;
+            this.btnFolder1.Click += new System.EventHandler(this.btnFolder1_Click);
+            // 
+            // labelPath1
+            // 
+            this.labelPath1.AutoSize = true;
+            this.labelPath1.Location = new System.Drawing.Point(61, 105);
+            this.labelPath1.Name = "labelPath1";
+            this.labelPath1.Size = new System.Drawing.Size(80, 16);
+            this.labelPath1.TabIndex = 1;
+            this.labelPath1.Text = "folder 1 path";
+            this.labelPath1.Click += new System.EventHandler(this.labelHelloWorld_Click);
+            // 
+            // labelPath2
+            // 
+            this.labelPath2.AutoSize = true;
+            this.labelPath2.Location = new System.Drawing.Point(61, 247);
+            this.labelPath2.Name = "labelPath2";
+            this.labelPath2.Size = new System.Drawing.Size(80, 16);
+            this.labelPath2.TabIndex = 3;
+            this.labelPath2.Text = "folder 2 path";
+            this.labelPath2.Click += new System.EventHandler(this.labelPath2_Click);
+            // 
+            // btnFolder2
+            // 
+            this.btnFolder2.Location = new System.Drawing.Point(43, 171);
+            this.btnFolder2.Name = "btnFolder2";
+            this.btnFolder2.Size = new System.Drawing.Size(116, 45);
+            this.btnFolder2.TabIndex = 2;
+            this.btnFolder2.Text = "select folder 2";
+            this.btnFolder2.UseVisualStyleBackColor = true;
+            this.btnFolder2.Click += new System.EventHandler(this.btnFolder2_Click);
+            // 
+            // SelectFolder1
+            // 
+            this.SelectFolder1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 487);
+            this.Controls.Add(this.labelPath2);
+            this.Controls.Add(this.btnFolder2);
+            this.Controls.Add(this.labelPath1);
+            this.Controls.Add(this.btnFolder1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFolder1;
+        private System.Windows.Forms.Label labelPath1;
+        private System.Windows.Forms.Label labelPath2;
+        private System.Windows.Forms.Button btnFolder2;
+        private System.Windows.Forms.FolderBrowserDialog SelectFolder1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Event handler for button click
+        private void btnFolder1_Click(object sender, EventArgs e)
+        {
+            // Create and configure a FolderBrowserDialog
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select a folder";
+
+                // Show the dialog and check if the user clicked OK
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    // Display the selected folder path in the label
+                    labelPath1.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+        }
+
+        // Event handler for label click
+        private void labelHelloWorld_Click(object sender, EventArgs e)
+        {
+            labelPath1.Text = "Goodbye World!";
+        }
+
+        // Event handler for FolderBrowserDialog HelpRequest (optional)
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is the folder browser dialog help.");
+        }
+
+        private void btnFolder2_Click(object sender, EventArgs e)
+        {
+            // Create and configure a FolderBrowserDialog
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = "Select a folder";
+
+                // Show the dialog and check if the user clicked OK
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    // Display the selected folder path in the label
+                    labelPath2.Text = folderBrowserDialog.SelectedPath;
+                }
+            }
+
+        }
+
+        private void labelPath2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
